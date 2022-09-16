8fcefed5596067116268462b130fd7d617363239
JasonBeagle <skipskipperson385@gmail.com>
2022-09-16 07:35:30 -0700

    project0x00-C-task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ *will print "with proper grammar, but the outcome is a piece art"
+ */
+
+int main(void);
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	(return 0);
+}
