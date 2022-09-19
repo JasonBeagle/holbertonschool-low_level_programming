1726c397eaa99e9147f168b32d9cc48a0ccc395a
JasonBeagle <skipskipperson385@gmail.com>
2022-09-19 09:12:18 -0700

    project0x01-task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints letters a-z in lower and upper case
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar ('\n');
+	return (0);
+}
