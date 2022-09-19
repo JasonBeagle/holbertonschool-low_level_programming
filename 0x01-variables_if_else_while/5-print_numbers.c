e472c699f42bf38936f158d17861f239b464a20b
JasonBeagle <skipskipperson385@gmail.com>
2022-09-19 11:31:53 -0700

    project0x01-task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - main will print numbers until 10 starting at 0
+ *return: always success 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
