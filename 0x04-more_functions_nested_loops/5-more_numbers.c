96264e9a6a849f7f98dbb747626e9947fe0b1e2c
JasonBeagle <skipskipperson385@gmail.com>
2022-09-24 07:35:31 -0700

    project0x04-C-functions-task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,18 +3,18 @@
  *more_numbers - prints 0-14 ten times
  *
  */
-void more_numbers(void);
+void more_numbers(void)
 {
 	int num, till;
 
 	for (till = 0; till <= 9; till++)
 	{
-		for (num = 0; num <= 14 num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 				_putchar((num / 10) + '0');
-			_putchar((num % 0) + '0');
+			_putchar((num % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
