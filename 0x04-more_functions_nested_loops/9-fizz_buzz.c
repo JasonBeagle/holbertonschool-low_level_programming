5a0fe41901f5416cde0feade405053e2d7ef9242
JasonBeagle <skipskipperson385@gmail.com>
2022-09-26 09:51:34 -0700

    projects 0x04 & 0x05

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,30 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
 *main - print 1- 100, multiples of 3 exchange for Fizz,
 *	multiples of 5 changes to Buzz.
 *
 *Return: always 0 (success).
 */
+
 int main(void)
 {
 	int num;
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0)
+		if ((num % 3) == 0 && (num % 5) != 0)
 			printf("Fizz");
-
-		else if ((num % 5) == 0)
+		else if ((num % 5) == 0 && (num % 3) != 0)
 			printf("Buzz");
 
-		else
+		else if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		else if (num == 1)
 			printf("%d", num);
-
-		if (num == 100)
-			continue;
-		printf(" ");
+		else
+			printf(" %d", num);
 	}
 
 	printf("\n");
