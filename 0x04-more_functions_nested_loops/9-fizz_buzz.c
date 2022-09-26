eb054395e0c9f1e47de6e5f35906789e02d54e3f
JasonBeagle <skipskipperson385@gmail.com>
2022-09-26 09:54:40 -0700

    projects 0x04 & 0x05

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3) == 0 && (num % 5) != 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if ((num % 5) == 0 && (num % 3) != 0)
-			printf("Buzz");
+			printf(" Buzz");
 
 		else if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (num == 1)
 			printf("%d", num);
 		else
