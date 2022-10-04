c16c308d078cc819d1d84e9701a9468f304b4c94
JasonBeagle <skipskipperson385@gmail.com>
2022-10-04 15:14:57 -0700

    project0x06-tasks

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+*leet - encodes a string to 1337
+*@s: string to be encoded
+*
+*Return: the final string
+*/
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
