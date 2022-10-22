264632d547bb0a6af2bfdd6fae80c25f5036865a
JasonBeagle <skipskipperson385@gmail.com>
2022-10-22 11:51:11 -0700

    project0x0F.C-Structures-task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - a structure logs information aboout dogs.
+*@name: first member, dogs name.
+*@age: second member, dogs age.
+*@owner: third member, dogs owner.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
