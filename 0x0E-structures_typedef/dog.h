6d7c314a97d008d991e6fdf1a33d245085f1decf
JasonBeagle <skipskipperson385@gmail.com>
2022-10-22 18:23:54 -0700

    project0x0F.C-Structures-tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,12 @@ struct dog
 	char *owner;
 };
 
-/** Initializes a variable of type struct dog */
+/**Initializes a variable of type struct dog**/
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+/**Prints struct dog**/
+void print_dog(struct dog *d);
+/**Defines a new type, dog_t of struct dog type**/
+typedef struct dog dog_t;
+/**Creates a new dog**/
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
