de2e99c797efaada5f8537c0859f2669363bca74
JasonBeagle <skipskipperson385@gmail.com>
2022-11-08 12:29:23 -0800

    Project0x14.C-MoreSinglyLinkedLists-task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * freelist_int - frees a linked list from memory.
+ * @head: pointer to linked list.
+ *
+ * Return: void, the great beyond, nothingness
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *setmefree;
+
+	while (head != NULL)
+	{
+		setmefree = head;
+		head = head->next;
+		free(setmefree);
+	}
+}
